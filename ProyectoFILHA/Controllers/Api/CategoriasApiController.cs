9fef9a7df756d3d7b3126508198aea49e31bff11
blackwero <43689361+blackwero@users.noreply.github.com>
2026-04-09 23:30:48 -0600

    Add files via upload

---
+++ ProyectoFILHA/Controllers/Api/CategoriasApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFILHA.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasApiController : ControllerBase
+    {
+        private static List<string> categorias = new List<string>
+        {
+            "Maquillaje",
+            "Cuidado de la piel",
+            "Fragancias",
+            "Cabello"
+        };
+
+        [HttpGet]
+        public IActionResult GetCategorias()
+        {
+            return Ok(categorias);
+        }
+    }
+}
---
+++ ProyectoFILHA/Controllers/Api/CosmeticosApiController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFILHA.Models;
+using ProyectoFILHA.Models.Entidades;
+
+namespace ProyectoFILHA.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CosmeticosApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CosmeticosApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/cosmeticos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cosmetico>>> GetCosmeticos()
+        {
+            return await _context.Cosmeticos.ToListAsync();
+        }
+
+        // GET: api/cosmeticos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cosmetico>> GetCosmetico(int id)
+        {
+            var cosmetico = await _context.Cosmeticos.FindAsync(id);
+
+            if (cosmetico == null)
+            {
+                return NotFound();
+            }
+
+            return cosmetico;
+        }
+
+        // POST: api/cosmeticos
+        [HttpPost]
+        public async Task<ActionResult<Cosmetico>> CreateCosmetico(Cosmetico cosmetico)
+        {
+            _context.Cosmeticos.Add(cosmetico);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCosmetico), new { id = cosmetico.Id }, cosmetico);
+        }
+    }
+}
